7f361d09d0762e5191645af8ef75c10c8d3e1d30
topblame <113543762+topblame@users.noreply.github.com>
2025-12-20 17:37:45 +0900

    mbti폴더 합침
    폴더합쳣음

---
+++ app/mbti/adapter/input/web/__init__.py
@@ -0,0 +1,3 @@
+from app.mbti.adapter.input.web.router_ai_question import router as mbti_test_ai_question_router
+
+__all__ = ["mbti_test_ai_question_router"]
--- app/mbti_test/adapter/input/web/__init__.py
+++
@@ -1,3 +0,0 @@
-from .router_ai_question import router as mbti_test_ai_question_router
-
-__all__ = ["mbti_test_ai_question_router"]
